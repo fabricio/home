2be3f4d5f0156df243d7611c0482c252f181d7d6
Fabrício Soares <eu@fabriciosoares.com>
2022-10-12 17:51:41 -0300

    fix: optimize code

--- routes/api/title.ts
+++ routes/api/title.ts
@@ -1,17 +1,17 @@
 import { TITLES } from './locale.ts';
 
+export const {pt, en} = TITLES;
+export const random = (val:Array<string>) => Math.floor(Math.random() * val.length);
 export const checkLocale = ():string => Intl.DateTimeFormat().resolvedOptions().locale;
 
-export const Title = () => {
-  if (checkLocale() === 'pt-BR') {
-    const randomIndex = Math.floor(Math.random() * TITLES['pt-BR'].length);
-    const title = TITLES['pt-BR'][randomIndex];
-    return title;
-  }
-  else{
-    const randomIndex = Math.floor(Math.random() * TITLES['en-US'].length);
-    const title = TITLES['en-US'][randomIndex];
-    return title;
+// export const Title = () => {
 
-  }
-};
+//   if (checkLocale() === 'pt-BR') {
+//     const randomIndex = Math.floor(Math.random() * pt.length);
+//     return pt[randomIndex];
+//   }
+//   else{
+//     const randomIndex = Math.floor(Math.random() * en.length);
+//     return en[randomIndex];
+//   }
+// };
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,23 @@
 
-import { Title, checkLocale } from './api/title.ts'
+import { pt, en, random, checkLocale } from './api/title.ts'
 import { HeadComponent } from '../components/Head.tsx'
 import { Footer } from '../components/Footer.tsx'
 
-
 const name ='Fabrício';
-const title = Title;
-
-export const locale = checkLocale();
+// const title = Title;
+// {title()}
+const locale = checkLocale();
 
 export default function Home() {
-
   return (
     <>
       <HeadComponent />
-      {locale}
+      {/* {locale} */}
       <body class="bg-bg font-sans text-black">
         <main class="p-4 mx-auto max-w-screen-sm flex justify-center items-center h-screen flex-wrap flex-col">
           <div class=" w-full">
             <h1 class="font-bold text-5xl"><a href="/contact" class=""><span class="">{name}</span></a></h1>
-            <span class="text-2xl"><a href="/frontend" class="">Front-end</a>, UX {locale === 'pt-BR'? 'e': 'and'} {title()}.</span>
+            <span class="text-2xl"><a href="/frontend" class="">Front-end</a>, UX {locale === 'pt-BR'? 'e': 'and'} {locale === 'pt-BR'? pt[random(pt)] : en[random(en)]}.</span>
           </div>
           <ul class="w-full mt-5 font-noto">
             <li class="text-xs"><a href="http://github.com/fabricio/" class="text-base font-medium">/fabricio</a> on github</li>
@@ -29,6 +27,5 @@ export default function Home() {
         </main>
       </body>
     </>
-
   );
 }
