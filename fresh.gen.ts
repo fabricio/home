1d7d78dc180dd972cb88778804464428ad3bc1c5
Fabrício Soares <eu@fabriciosoares.com>
2023-09-10 16:06:24 -0300

    fix(remove): remove routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,17 +5,13 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/locale.ts";
 import * as $1 from "./routes/api/title.ts";
-import * as $2 from "./routes/contact.tsx";
-import * as $3 from "./routes/frontend.tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $3 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/api/locale.ts": $0,
     "./routes/api/title.ts": $1,
-    "./routes/contact.tsx": $2,
-    "./routes/frontend.tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/index.tsx": $3,
   },
   islands: {},
   baseUrl: import.meta.url,
